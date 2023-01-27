fd95df529b0c33e40c8240d748aa4ad7a52bb8c4
Candice Jovon Juarez <candice.artist@gmail.com>
2023-01-27 15:23:57 -0500

    created file

---
+++ app.js
@@ -0,0 +1,39 @@
+//alerts&prompts
+
+// Variables
+let introduction = 'Welcome To My Site'
+let username = 'PerScholas Holla Dollah'
+let location = 'NewYork'
+let date = 'Jan. 27, 2023'
+
+//array
+const students = ['Dana', 'DJ'. 'Deja', 'Divine']
+
+//object
+let favDay = {
+    day: 'Friday',
+    weather:'Sunny',
+    activity: 'Art Museum',
+    homework: false,
+    timeClassEnds: 4
+
+}
+
+
+//Conditionals
+
+
+//Functions
+
+const intoSite = () => {
+    let guessAge = prompt('Are you 18 and over?')
+
+    if(guessAge) = document.querySelector('.container')
+    //target class by name 
+    mainBox.innerHtml = introduction
+} else{
+    let ourPgae = document.querySelector
+}
+
+
+//querySelectors allows you to find an element based on its name in you query or index html file
